6b746cf35606b89e4514401d55d1b714f2c6ca69
Ravi Sharma <ravisha@chalmers.se>
2026-01-07 23:47:43 +0100

    #2 add isNumber and isLetter helpers

--- main.go
+++ main.go
@@ -11,6 +11,22 @@ func tokenizer(input string) []token {
 	return []token{}
 }
 
+func isNumber(char string) bool {
+	if char == "" {
+		return false
+	}
+	n := []rune(char)[0]
+	return n >= '0' && n <= '9'
+}
+
+func isLetter(char string) bool {
+	if char == "" {
+		return false
+	}
+	n := []rune(char)[0]
+	return n >= 'a' && n <= 'z'
+}
+
 func main() {
 	input := "(add 2 (subtract 10 5))"
 	fmt.Println(input)
